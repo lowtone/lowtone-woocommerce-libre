948f479a0200137a58f30fc01818bd929a61d13f
Paul van der Meijs <paul@lowtone.nl>
2013-04-28 23:41:31 +0200

    Add styles.

--- assets/templates/index.xsl
+++ assets/templates/index.xsl
@@ -12,16 +12,17 @@
 	<xsl:template match="post[type='product']">
 		<xsl:param name="single" />
 		
-		<article id="{name}">
+		<article id="{name}" itemscope="itemscope" itemtype="http://schema.org/Product">
 			<xsl:attribute name="class">
 				<xsl:text>post type-</xsl:text><xsl:value-of select="type" />
 				<xsl:if test="$single">
 					<xsl:text> single</xsl:text>
 				</xsl:if>
 			</xsl:attribute>
-			<div class="content">
-				<xsl:value-of select="content" disable-output-escaping="yes" />
-			</div>
+
+			<a href="{permalink}">
+				<h3><xsl:value-of select="title" /></h3>
+			</a>
 		</article>
 	</xsl:template>
 
--- lowtone-woocommerce-libre.php
+++ lowtone-woocommerce-libre.php
@@ -19,7 +19,7 @@
 namespace lowtone\woocommerce\libre {
 
 	use lowtone\Util,
-		lowtone\content\packages\plugins\Plugin;
+		lowtone\content\packages\Package;
 
 	// Includes
 	
@@ -28,46 +28,78 @@ namespace lowtone\woocommerce\libre {
 
 	// Init
 
-	Plugin::init(array(
-			Plugin::INIT_PACKAGES => array("lowtone", "lowtone\\woocommerce"),
-			Plugin::INIT_MERGED_PATH => __NAMESPACE__
+	Package::init(array(
+			Package::INIT_PACKAGES => array("lowtone", "lowtone\\woocommerce"),
+			Package::INIT_MERGED_PATH => __NAMESPACE__,
+			Package::INIT_SUCCESS => function() {
+
+				add_action("init", function() {
+					if (!checkWooCommerce())
+						return;
+
+					// Config
+
+					if (!defined("LOWTONE_WOOCOMMERCE_LIBRE_APPEND_TEMPLATE"))
+						define("LOWTONE_WOOCOMMERCE_LIBRE_APPEND_TEMPLATE", true);
+
+					if (!defined("LOWTONE_WOOCOMMERCE_LIBRE_INCLUDE_STYLES"))
+						define("LOWTONE_WOOCOMMERCE_LIBRE_INCLUDE_STYLES", true);
+
+					remove_filter("template_include", array($GLOBALS["woocommerce"], "template_loader"));
+
+					add_filter("post_document_content", function($content) {
+						if (!(is_single() && get_post_type() == "product"))
+							return $content;
+
+						return Util::catchOutput(function() {
+							while (have_posts()) {
+								the_post();
+
+								woocommerce_get_template_part("content", "single-product");
+							}
+						});
+					});
+
+					// Append template
+
+					if (LOWTONE_WOOCOMMERCE_LIBRE_APPEND_TEMPLATE) {
+
+						add_filter("libre_append_templates", function($templates, $template) {
+							$templates[] = __DIR__ . "/assets/templates/index.xsl";
+
+							return $templates;
+						}, 10, 2);
+
+					}
+
+					// Include styles
+					
+					if (LOWTONE_WOOCOMMERCE_LIBRE_INCLUDE_STYLES) {
+
+						$stylesheet = NULL;
+
+						$path = "styles/woocommerce.css";
+
+						foreach (array(get_stylesheet_directory(), get_template_directory()) as $i => $dir) {
+							if (!is_file($dir . DIRECTORY_SEPARATOR . $path))
+								continue;
+							
+							$stylesheet = (0 == $i ? get_stylesheet_directory_uri() : get_template_directory_uri()) . "/" . $path;
+							
+							break;
+						}
+
+						if (!$stylesheet)
+							$stylesheet = plugins_url("/assets/" . $path, __FILE__);
+
+						wp_enqueue_style("lowtone_woocommerce_libre", $stylesheet, array("woocommerce_frontend_styles"));
+
+					}
+
+				});
+
+			}
 		));
-
-	add_action("init", function() {
-		if (!checkWooCommerce())
-			return;
-
-		// Config
-
-		if (!defined("LOWTONE_WOOCOMMERCE_LIBRE_APPEND_TEMPLATE"))
-			define("LOWTONE_WOOCOMMERCE_LIBRE_APPEND_TEMPLATE", true);
-
-		remove_filter("template_include", array($GLOBALS["woocommerce"], "template_loader"));
-
-		add_filter("post_document_content", function($content) {
-			if (!(is_single() && get_post_type() == "product"))
-				return $content;
-
-			return Util::catchOutput(function() {
-				while (have_posts()) {
-					the_post();
-
-					woocommerce_get_template_part("content", "single-product");
-				}
-			});
-		});
-
-		if (LOWTONE_WOOCOMMERCE_LIBRE_APPEND_TEMPLATE) {
-
-			add_filter("libre_append_templates", function($templates, $template) {
-				$templates[] = __DIR__ . "/assets/templates/index.xsl";
-
-				return $templates;
-			}, 10, 2);
-
-		}
-
-	});
 
 	// Functions
 
